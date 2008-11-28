28381d1b7b9927b600bead2bd2bddcef997b1a0d
Tanguy Krotoff <tkrotoff@gmail.com>
2008-11-28 05:39:00 +0000

    Phonon: extended_chapters_titles.patch

--- 3rdparty/phonon/objectdescription.h
+++ 3rdparty/phonon/objectdescription.h
@@ -65,6 +65,8 @@ namespace Phonon
         EffectType,
         AudioChannelType,
         SubtitleType,
+        ChapterType,
+        TitleType,
 
         /**
          * Audio capture devices. This can be soundcards (with different drivers), soundservers or
@@ -310,6 +312,8 @@ typedef ObjectDescription<EffectType> EffectDescription;
 #ifndef QT_NO_PHONON_MEDIACONTROLLER
 typedef ObjectDescription<AudioChannelType> AudioChannelDescription;
 typedef ObjectDescription<SubtitleType> SubtitleDescription;
+typedef ObjectDescription<ChapterType> ChapterDescription;
+typedef ObjectDescription<TitleType> TitleDescription;
 #endif //QT_NO_PHONON_MEDIACONTROLLER
 
 } //namespace Phonon
@@ -333,8 +337,12 @@ Q_DECLARE_METATYPE(Phonon::EffectDescription)
 #ifndef QT_NO_PHONON_MEDIACONTROLLER
 Q_DECLARE_METATYPE(Phonon::AudioChannelDescription)
 Q_DECLARE_METATYPE(Phonon::SubtitleDescription)
+Q_DECLARE_METATYPE(Phonon::ChapterDescription)
+Q_DECLARE_METATYPE(Phonon::TitleDescription)
 Q_DECLARE_METATYPE(QList<Phonon::AudioChannelDescription>)
 Q_DECLARE_METATYPE(QList<Phonon::SubtitleDescription>)
+Q_DECLARE_METATYPE(QList<Phonon::ChapterDescription>)
+Q_DECLARE_METATYPE(QList<Phonon::TitleDescription>)
 #endif //QT_NO_PHONON_MEDIACONTROLLER
 
 QT_END_HEADER
