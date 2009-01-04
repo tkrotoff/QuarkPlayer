da571e43baf57612965ffd14340643a54363550f
Tanguy Krotoff <tkrotoff@gmail.com>
2009-01-04 11:05:38 +0000

    New Debian script for a future .deb package, create using dh_make -n -s -e

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(quarkplayer):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="quarkplayer" command="/usr/bin/quarkplayer"
---
+++ debian/quarkplayer.default.ex
@@ -0,0 +1,10 @@
+# Defaults for quarkplayer initscript
+# sourced by /etc/init.d/quarkplayer
+# installed at /etc/default/quarkplayer by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
