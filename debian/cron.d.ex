173ccfc6db3f7975100594e28c22b6ac5df850fd
Tanguy Krotoff <tkrotoff@gmail.com>
2009-01-04 12:11:34 +0000

    Remove *.ex and *.EX from the Debian package (does not work yet)

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the quarkplayer package
-#
-0 4	* * *	root	[ -x /usr/bin/quarkplayer_maintenance ] && /usr/bin/quarkplayer_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(quarkplayer):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="quarkplayer" command="/usr/bin/quarkplayer"
