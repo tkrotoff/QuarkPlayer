d1cf6a0ae4615fc4658ce6e37f689e18eaaa84f7
Tanguy Krotoff <tkrotoff@gmail.com>
2009-09-22 16:21:45 +0000

    Add scripts for Qt-4.6.0-tp1 x86 and x64 QuarkPlayer compiles perfectly under Visual C++ 2008 64bits

---
+++ build/setenv_qt-4.6.0-tp1-msvc90-x64.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo Setting up Qt 4.6.0-tp1...
+set QTDIR=C:\Qt\4.6.0-tp1
+set PATH=%QTDIR%\bin;%PATH%
+set QMAKESPEC=win32-msvc2008
+
+echo Setting up CMake 2.6...
+rem Remove a substring using string substitution
+rem See http://www.dostips.com/DtTipsStringManipulation.php
+set PATH=%PATH:C:\Program Files (x86)\CMake 2.6\bin=%
+set PATH=C:\Program Files (x86)\CMake 2.6\bin;%PATH%
+
+rem Must be the last line in this script: create a new shell/prompt
+echo Setting up Microsoft Visual Studio 2008 (MSVC90)...
+call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
