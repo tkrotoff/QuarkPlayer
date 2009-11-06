041055d740d4953ec8589bf648f4c46875a74a4a
Tanguy Krotoff <tkrotoff@gmail.com>
2009-11-06 17:30:47 +0000

    Add FindSubtitlesTest (not done yet)

---
+++ tests/quarkplayer-plugins/findsubtitles/FindSubtitlesTest.h
@@ -0,0 +1,47 @@
+/*
+ * QuarkPlayer, a Phonon media player
+ * Copyright (C) 2008-2009  Tanguy Krotoff <tkrotoff@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FINDSUBTITLESTEST_H
+#define FINDSUBTITLESTEST_H
+
+#include <QtTest/QtTest>
+
+/**
+ * Test class for FindSubtitles plugin.
+ *
+ * @see FindSubtitlesWindow
+ * @author Tanguy Krotoff
+ */
+class FindSubtitlesTest : public QObject {
+	Q_OBJECT
+private slots:
+
+	/** Called before the first testfunction is executed. */
+	void initTestCase();
+
+	/** Called after the last testfunction was executed. */
+	void cleanupTestCase();
+
+	/** Called before each testfunction is executed. */
+	void init();
+
+	/** Called after every testfunction. */
+	void cleanup();
+};
+
+#endif	//FINDSUBTITLESTEST_H
