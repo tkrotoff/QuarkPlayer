3a3d725a9a9e4604c3bec4e82143573789e5f8f4
Tanguy Krotoff <tkrotoff@gmail.com>
2009-12-31 12:58:02 +0000

    Update for CMake 2.8

--- build/setenv_qt-4.6-msvc90-x86.bat
+++ build/setenv_qt-4.6-msvc90-x86.bat
@@ -5,11 +5,11 @@ set QTDIR=C:\Qt\4.6.0
 set PATH=%QTDIR%\bin;%PATH%
 set QMAKESPEC=win32-msvc2008
 
-echo Setting up CMake 2.6...
+echo Setting up CMake 2.8...
 rem Remove a substring using string substitution
 rem See http://www.dostips.com/DtTipsStringManipulation.php
-set PATH=%PATH:C:\Program Files\CMake 2.6\bin=%
-set PATH=C:\Program Files\CMake 2.6\bin;%PATH%
+set PATH=%PATH:C:\Program Files\CMake 2.8\bin=%
+set PATH=C:\Program Files\CMake 2.8\bin;%PATH%
 
 rem Must be the last line in this script: create a new shell/prompt
 echo Setting up Microsoft Visual Studio 2008 (MSVC90)...
